3aa4ce974449606449612d761fc32cc2bb3c2600
Arnaudsyn <bladou@gmail.com>
2024-08-16 17:28:53 +0200

    maj3

--- dist/script.js
+++ dist/script.js
@@ -53,19 +53,17 @@ function init() {
     Measure === null || Measure === void 0 ? void 0 : Measure.addEventListener('click', HandleClick);
 }
 function checkPassword() {
-    document.getElementById("login-prompt").style.display = "none";
-    /*const userPassword = btoa(prompt("Ce site requiert un mot de passe, entre le ci-dessous:")!);
-    const MainContent = document.querySelector('main');
-
-    MainContent!.style.display = 'none';
-
+    var userPassword = btoa(prompt("Ce site requiert un mot de passe, entre le ci-dessous:"));
+    var MainContent = document.querySelector('main');
+    MainContent.style.display = 'none';
     if (userPassword === correctPassword) {
-        MainContent!.style.display = "block";
-        document.getElementById("login-prompt")!.style.display = "none";
-    } else {
+        MainContent.style.display = "block";
+        document.getElementById("login-prompt").style.display = "none";
+    }
+    else {
         alert("Mot de passe incorrect, merci de réessayer.");
         location.reload();
-    }*/
+    }
 }
 function HandleClick(e) {
     var target = e.currentTarget;
